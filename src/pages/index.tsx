230706f66806b77546d0566d4d5966f0a2f38119
kristiandavidd <kristiandavid644@gmail.com>
2023-10-27 02:05:16 +0700

    fix: button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <div className='w-full h-screen  flex flex-col justify-center items-center '>
       <h1 className="font-display max-w-[704px] bg-gradient-to-br from-blue-900 to-blue-500 bg-clip-text p-4 pb-0 text-center text-4xl font-bold tracking-[-0.02em] text-slate-900 text-transparent drop-shadow-sm m-4">Selamat Datang di Sistem informasi <br />Informatika Universitas Diponegoro!</h1>
       <p className='m-2'>Silakan masuk untuk mendapatkan akses.</p>
-      <a href="/login" type="button" className="text-white bg-blue-500 hover:bg-blue-400 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-500 dark:hover:bg-blue-500 focus:outline-none dark:focus:ring-blue-200 w-1/5 text-center">Login</a>
+      <a href="login" type="button" className="text-white bg-blue-500 hover:bg-blue-400 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-500 dark:hover:bg-blue-500 focus:outline-none dark:focus:ring-blue-200 w-1/5 text-center">Login</a>
       
     </div>
     </div>
