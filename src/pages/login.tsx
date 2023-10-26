4d8cb67e53d20da9bdc784d73117072826b9a8c3
kristiandavidd <kristiandavid644@gmail.com>
2023-10-26 07:32:06 +0700

    feat:file

---
+++ src/pages/login.tsx
@@ -0,0 +1,18 @@
+export default function Login() {
+    return (
+        <div className="w-full h-screen">
+            <div
+                className="bg-center bg-fit w-screen h-1/2 min-h-0 m-font bg-no-repeat"
+                style={{
+                backgroundImage: "url('../../DSC7133.jpg')",
+                }}
+            >
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+    
+}
