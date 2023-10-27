f187bb42c0f409f28f039ec7fe8f9b76de505862
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2023-10-27 08:17:01 +0700

    feat: edit modal

---
+++ src/config/mahasiswa_profile_config.tsx
@@ -0,0 +1,13 @@
+export const profileData = {
+    name: "Nama",
+    nim: "NIM",
+    email: "Email",
+    angkatan: "Angkatan",
+    semester: "Semester",
+    jalurMasuk: "Jalur Masuk",
+    nomorTelepon: "Nomor Telepon",
+    alamat: "Alamat",
+    kabupatenKota: "Kab/Kota",
+    provinsi: "Provinsi",
+  };
+  
