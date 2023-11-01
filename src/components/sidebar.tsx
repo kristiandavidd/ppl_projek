f5f1c813d04fa64176f0bbd40c133fb9f2199503
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2023-11-01 14:41:12 +0700

    feat: making a new generate account page

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,41 +1,48 @@
-import {sidebarData} from "@/config/sidebar_config";
+import { sidebarData } from "@/config/sidebar_config";
 import NavButton from "./nav_button";
-import {UserCircle} from "tabler-icons-react"
+import { UserCircle } from "tabler-icons-react";
+import { Button } from "@material-tailwind/react";
+import { FaSignOutAlt } from "react-icons/fa";
 
-type userData = { 
-    role: string;
-    name: string;
-    idNumber: string;
-}
+type userData = {
+  role: string;
+  name: string;
+  idNumber: string;
+};
 
 interface userProps {
-    data: userData;
+  data: userData;
 }
 
 interface SidebarData {
-    [key: string]: { title: string; link: string; svg: JSX.Element }[];
+  [key: string]: { title: string; link: string; svg: JSX.Element }[];
 }
 
 export default function Sidebar({ data }: userProps) {
-    const { role, name, idNumber } = data;
-    const sidebarRoleData = (sidebarData as unknown as SidebarData)[role] || [];
+  const { role, name, idNumber } = data;
+  const sidebarRoleData = (sidebarData as unknown as SidebarData)[role] || [];
 
-    return (
-        <div className='flex-col items-center hidden p-10 mb-20 bg-blue-500 w-1/8 lg:mb-0 lg:flex'>
-            <UserCircle color="white" size={96} strokeWidth={1} />
-            <div className='flex items-center justify-center px-2 py-2 mx-auto mb-2 mt-5 rounded-md w-[200px] items bg-blue-300'>
-                <p className='text-sm font-semibold text-center'>{name}</p>
-            </div>
-            <div className="mb-10">
-                <p className='text-sm text-center text-white'>{idNumber}</p>    
-            </div>
-            <div className="w-full">
-                <ul className="w-full flex flex-col justify-start">
-                    {sidebarRoleData.map((data) => {
-                        return <NavButton data={data} key={data.title} />;
-                    })}
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex-col items-center hidden p-10 mb-20 bg-blue-500 w-1/8 lg:mb-0 lg:flex h-screen overflow-x-visible">
+      <UserCircle color="white" size={96} strokeWidth={1} />
+      <div className="flex items-center justify-center px-2 py-2 mx-auto mb-2 mt-5 rounded-md w-[200px] items bg-blue-300">
+        <p className="text-sm font-semibold text-center">{name}</p>
+      </div>
+      <div className="mb-10">
+        <p className="text-sm text-center text-white">{idNumber}</p>
+      </div>
+      <div className="w-full">
+        <ul className="w-full flex flex-col justify-start">
+          {sidebarRoleData.map((data) => {
+            return <NavButton data={data} key={data.title} />;
+          })}
+        </ul>
+      </div>
+      <div className="mt-auto">
+        <Button color="white" className=" flex items-center">
+          <FaSignOutAlt className="mr-2" /> Logout
+        </Button>
+      </div>
+    </div>
+  );
 }
