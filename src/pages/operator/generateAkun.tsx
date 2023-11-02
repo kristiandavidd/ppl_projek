317a4116a75aa46ba94276968460f2d2bba3b6d5
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2023-11-02 19:10:23 +0700

    feat: fix generateAkun

--- src/pages/operator/generateAkun.tsx
+++ src/pages/operator/generateAkun.tsx
@@ -31,8 +31,11 @@ export default function DataDoswal() {
 
       const maxFileSize = 50 * 1024 * 1024;
 
-      if (fileExtension.toLowerCase() !== "csv") {
-        alert("Hanya file dengan format CSV yang dapat diunggah!");
+      if (
+        fileExtension.toLowerCase() !== "csv" &&
+        fileExtension.toLowerCase() !== "xlsx"
+      ) {
+        alert("Hanya file dengan format CSV atau XLSX yang dapat diunggah!");
         event.target.value = null; // Reset input file
         setUploadedFileName(null);
       } else if (fileSize > maxFileSize) {
@@ -59,8 +62,11 @@ export default function DataDoswal() {
 
       const maxFileSize = 50 * 1024 * 1024;
 
-      if (fileExtension.toLowerCase() !== "csv") {
-        alert("Hanya file dengan format CSV yang dapat diunggah!");
+      if (
+        fileExtension.toLowerCase() !== "csv" &&
+        fileExtension.toLowerCase() !== "xlsx"
+      ) {
+        alert("Hanya file dengan format CSV atau XLSX yang dapat diunggah!");
         event.target.value = null; // Reset input file
         setUploadedFileName(null);
       } else if (fileSize > maxFileSize) {
@@ -128,7 +134,7 @@ export default function DataDoswal() {
               </Typography>
             </div>
             <p className="text-sm text-gray-500">
-              Upload file dengan format .csv
+              Upload file dengan format .csv atau .xlsx
             </p>
 
             <Card className="mt-6 w-full">
@@ -150,9 +156,9 @@ export default function DataDoswal() {
                       >
                         <path
                           stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                         />
                       </svg>
@@ -161,7 +167,7 @@ export default function DataDoswal() {
                         or drag and drop
                       </p>
                       <p className="text-xs text-gray-500 dark:text-gray-400">
-                        CSV (Maks. 50 MB)
+                        CSV or XLSX (Max. 50 MB)
                       </p>
                     </div>
                     <input
@@ -169,7 +175,7 @@ export default function DataDoswal() {
                       type="file"
                       className="hidden"
                       onChange={handleFileUpload}
-                      accept=".csv"
+                      accept=".csv, .xlsx"
                     />
                   </label>
                 </div>
