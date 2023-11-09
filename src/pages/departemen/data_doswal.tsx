2e909e0f53e6301be6c04a9385b9216246df6e6a
RadityaIH <radityadave10@gmail.com>
2023-11-09 20:09:33 +0700

    feat: search bar doswal and dept

--- src/pages/departemen/data_doswal.tsx
+++ src/pages/departemen/data_doswal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { EmptyLayout } from '@/components/layout'
 import Sidebar from '@/components/sidebar'
+import { Input, Select, Option } from '@material-tailwind/react';
 import {Bell} from 'tabler-icons-react'
 
 export default function DataDoswal() {
@@ -17,6 +18,26 @@ export default function DataDoswal() {
           <div className='flex w-full justify-between'>
               <h2 className='font-semibold text-lg'>Data Dosen Wali</h2>
               <Bell size={28} strokeWidth={1.5}></Bell>
+          </div>
+          <div className='bg-blue-50 shadow-lg shadow-blue-500/20 rounded-lg p-4 mt-5'>
+              <div className='flex'>
+                  <div className='flex w-5/6'>
+                      <Input
+                          color='blue'
+                          label='Cari Dosen Wali'
+                          type='text'
+                          className='border border-gray-300 rounded-md'>
+                      </Input>
+                  </div>
+                  <div className='w-1/4 flex items-center justify-center rounded-md ml-3'>
+                      <Select variant="outlined" label="Urutkan">
+                          <Option>NIM</Option>
+                          <Option>Angkatan</Option>
+                      </Select>
+                  </div>
+              </div>
+              <div className='mt-4'>
+              </div>
           </div>
         </div>
       </div>
